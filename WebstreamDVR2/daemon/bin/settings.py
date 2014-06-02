a1c8fc54acb44983ce44c147952ac9dbc2dc802b
Stephen Jackson <scj7t4@mst.edu>
2014-06-02 14:55:15 -0500

    Reorganizing

--- WebstreamDVR2/daemon/bin/settings.py
+++
@@ -1,4 +0,0 @@
-from datetime import timedelta
-
-SCHEDULE_URL = "http://kmnr.org/api/schedule/"
-RIPPER_COMMAND = "icecream --name='{target}{show}/{show}-%Y-%m-%d' --stop='{duration}min' -q {stream}"
