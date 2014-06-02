d5229d050b00f5ebf1eba7ee6fd6af54b61c0aad
Stephen Jackson <scj7t4@mst.edu>
2014-06-02 15:26:29 -0500

    Add in distutils stuff

---
+++ webstreamd/setup.py
@@ -0,0 +1,68 @@
+##############################################################################
+#
+# Copyright (c) 2014 Stephen Jackson
+# All Rights Reserved.
+#
+##############################################################################
+
+import os
+import sys
+
+py_version = sys.version_info[:2]
+
+if py_version < (2, 6):
+    raise RuntimeError('On Python 2, Supervisor requires Python 2.6 or later')
+elif (3, 0) <= py_version
+    raise RuntimeError('Not for Python 3 (yet)')
+
+requires = []
+tests_require = []
+    
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except:
+    README = """\
+Webstreamd is a utility for recording webstream according to a schedule\
+"""
+    CHANGES = ''
+
+CLASSIFIERS = [
+    'Topic :: System :: Monitoring',
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7"
+]
+
+version_txt = os.path.join(here, 'webstreamd/version.txt')
+webstreamd_version = open(version_txt).read().strip()
+
+dist = setup(
+    name='supervisor',
+    version=webstreamd_version,
+    license='',
+    url='http://kmnr.org',
+    description="A utility to automate recording webstreams based on a schedule",
+    long_description=README + '\n\n' +  CHANGES,
+    classifiers=CLASSIFIERS,
+    author="Stephen Jackson",
+    author_email="scj7t4@mst.edu",
+    maintainer="Stephen Jackson",
+    maintainer_email="scj7t4@mst.edu",
+    packages=find_packages(),
+    install_requires=requires,
+    extras_require={},
+    tests_require=tests_require,
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+     'console_scripts': [
+         'webstreamd-crontab = webstreamd.crontab:main',
+         'webstreamd-kmnr = webstreamd.coverters.kmnr:main',
+         'webstreamd-clean = webstreamd.clean:main',
+        ],
+    },
+)
--- webstreamd/webstreamd/clean.py
+++ webstreamd/webstreamd/clean.py
@@ -1,6 +1,8 @@
 import subprocess
 import settings
 
+settings = Settings()
+
 def clean():
     def do_directory(p):
         contents = os.listdir(p)
@@ -19,7 +21,16 @@ def clean():
             if now-d > timedelta(days=120):
                 util.lnp("Removing {}/{}".fomat(p,f))
                 subprocess.Popen(['rm',f],cwd=p).wait()
-    do_directory(settings.RECORDER_FOLDER)
+    do_directory(settings.RECORDINGDIR)
 
 if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        conf_file = sys.argv[1]
+    elif len(sys.argv) == 1:
+        print "Using default config {}".format(Settings.DEFAULTCONFIG)
+        conf_file = Settings.DEFAULTCONFIG
+    else:
+        print "Format: {} [CONFIG_FILE]".format(sys.argv[0])
+        exit(1)
+    settings.load_config(conf_file)
     clean()        
