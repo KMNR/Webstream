e2bab0092fbeb2562d0000d7676016c0e100ab4a
Doug McGeehan <djmvfb@mst.edu>
2017-03-06 23:35:29 -0600

    gonna actually use the settings file

--- webstreamd/webstreamd/verify.py
+++ webstreamd/webstreamd/verify.py
@@ -12,33 +12,46 @@ from crontab import safename
 import sys
 import os
 import urllib
+from settings import Settings
 
 
+settings = Settings()
 KMNR_ORG_SCHEDULE_URL = 'https://kmnr.org/api/schedule/'
-SHOW_RECORDINGS_DIR = '/var/www/'
 
 
 def _print_summary(show):
     print(show['title'].encode('utf-8'))
     show_DJs = [dj['name'] for dj in show['djs']]
     print('{djs}'.format(djs=' '.join(show_DJs)))
-    print('Stored in {}'.format(os.path.join(SHOW_RECORDINGS_DIR,
+    print('Stored in {}'.format(os.path.join(settings.RECORDINGDIR,
                                              safename(show['title']))))
     print('.'*80)
 
 
 def main():
+    """
+    If no config file is given as a commandline argument, uses the
+    default config file (Settings.DEFAULTCONFIG).
+    """
+    if len(sys.argv) == 2:
+        conf_file = sys.argv[1]
+    elif len(sys.argv) == 1:
+        conf_file = Settings.DEFAULTCONFIG
+    else:
+        print "Format: {} [CONFIG_FILE]".format(sys.argv[0])
+        exit(1)
+    settings.load_config(conf_file)
+
     schedule_raw = _load_schedule()
     shows = json.loads(schedule_raw)
     print('{} shows are defined in the schedule'.format(len(shows)))
 
     non_existent_shows = []
     for show in shows:
-        if not _verify_exists(show=show['title'], within=SHOW_RECORDINGS_DIR):
+        if not _verify_exists(show=show['title'], within=settings.RECORDINGDIR):
             _print_summary(show=show)
             non_existent_shows.append(show)
 
-    print('-'*80)
     print('{0} shows are not currently recorded (out of {1})'.format(
         len(non_existent_shows),
         len(shows)
@@ -53,30 +66,29 @@ def _load_schedule():
     Retrieve the show schedule in JSON format, either from KMNR.org or from
     the specified file.
     '''
-    if len(sys.argv) < 2:
-        # no show schedule file was provided as input
-        # retrieve it from kmnr.org
-        print('Downloading schedule from {}'.format(KMNR_ORG_SCHEDULE_URL))
-        response = urllib.urlopen(KMNR_ORG_SCHEDULE_URL)
-        data = response.read()
-        text = data.decode('utf-8')
+    print('Downloading schedule from {}'.format(KMNR_ORG_SCHEDULE_URL))
+    response = urllib.urlopen(KMNR_ORG_SCHEDULE_URL)
+    data = response.read()
+    text = data.decode('utf-8')
 
-    else:
-        schedule_file = sys.argv[1]
-        assert os.path.isfile(schedule_file), 'No file located at {}'.format(
-            schedule_file
-        )
-
-        print('Loading schedule from {}'.format(schedule_file))
-        with open(schedule_file) as f:
-            text = f.read()
+    ## Disabled functionality: if the local schedule file is passed as input,
+    ##  then load it.
+    # else:
+    #     schedule_file = sys.argv[1]
+    #     assert os.path.isfile(schedule_file), 'No file located at {}'.format(
+    #         schedule_file
+    #     )
+    #
+    #     print('Loading schedule from {}'.format(schedule_file))
+    #     with open(schedule_file) as f:
+    #         text = f.read()
 
     return text
 
 
 def _verify_exists(show, within):
     show_recording_directory = safename(show)
-    return os.path.isdir(os.path.join(SHOW_RECORDINGS_DIR,
+    return os.path.isdir(os.path.join(within,
                                       show_recording_directory))
 
 
